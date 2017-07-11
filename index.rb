651d1a390d90523e7cca825b5bb1608905fd6007
Yunosuke Katayama <purio.k1231@gmail.com>
2017-07-11 17:09:41 +0900

    done step 1

--- index.rb
+++ index.rb
@@ -1,12 +1,13 @@
 require "./drink"
+require "./machine"
 
 cola = Drink.new(name: "コーラ", price: 120, amount: 5)
-
+money = Money.new
 
 i = 0
 
 while i < 100 do 
-	options = ["コイン投入","ジュース一覧", "払い戻し"]
+	options = ["コイン投入","ジュース一覧", "終了する"]
 	index = 0
 
 	options.each do |option|
@@ -19,22 +20,20 @@ while i < 100 do
 	if choice == 0 
 		puts "お金を入れてください"
 		coin = gets.chomp.to_i
-		total_money = total_money.to_i + coin 
-		puts "合計金額は#{total_money}円です"
+
+		if coin == 10 || coin == 100 || coin == 500 || coin == 1000 
+			total_money = total_money.to_i + coin
+			puts "合計金額は#{total_money}円です"
+		else
+			change = coin 
+			puts "お釣りは#{change}円です"
+		end
+		
 	elsif choice == 1
 		puts "#{cola.info}"
 	else 
-		puts "お釣りは#{total_money}円です"
 		break
 	end
 	 i += 1 
 end
 
-=begin
-puts "お金を入れる"
-coin = gets.chomp.to_i
-total_money = total_money.to_i + coin 
-puts "合計金額は#{total_money}円です"
-i += 1 
-end 
-=end
--- machine.rb
+++
@@ -1,4 +0,0 @@
-class machine 
-
-
-end
